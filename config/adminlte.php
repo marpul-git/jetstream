4c568cb2679a381be379d2443d732fbfed35ef08
Mario Pulido Zuloaga <55960799+marpul-git@users.noreply.github.com>
2023-07-20 17:54:43 +0200

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,9 +14,9 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => '',
     'title_prefix' => '',
-    'title_postfix' => '',
+    'title_postfix' => '| Padel Rose ',
 
     /*
     |--------------------------------------------------------------------------
@@ -63,12 +63,12 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Padel</b>ROSE',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Admin Logo',
+    'logo_img_alt' => 'PadelRose Logo',
 
     /*
     |--------------------------------------------------------------------------
